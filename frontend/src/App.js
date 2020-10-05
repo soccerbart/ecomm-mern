f91127e515afc3dc41b4e0826cf707e59bd18173
soccerbart <bartholomew.wolinski@stonybrook.edu>
2020-10-05 17:19:52 -0400

    Implemented Redux for state, mongo for db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Homepage from './pages/homepage/Homepage';
+import Productpage from './pages/productpage/Productpage';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to my Ecomm Shop!</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={Homepage} exact />
+          <Route path="/product/:id" component={Productpage} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
