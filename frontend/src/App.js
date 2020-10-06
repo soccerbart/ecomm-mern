31779321cee8bb9477f66e81817552c72f956579
soccerbart <bartholomew.wolinski@stonybrook.edu>
2020-10-06 18:38:06 -0400

    Added backend as well as frontend authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,11 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Homepage from './pages/homepage/Homepage';
 import Productpage from './pages/productpage/Productpage';
+import Cartpage from './pages/cartpage/Cartpage';
+import Loginpage from './pages/loginpage/Loginpage';
+import Registerpage from './pages/registerpage/Registerpage';
+import Profilepage from './pages/profilepage/Profilepage';
+import Shippingpage from './pages/shippingpage/Shippingpage';
 
 const App = () => {
   return (
@@ -12,8 +17,13 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/" component={Homepage} exact />
+          <Route path="/login" component={Loginpage} />
+          <Route path="/register" component={Registerpage} />
+          <Route path="/shipping" component={Shippingpage} />
+          <Route path="/profile" component={Profilepage} />
           <Route path="/product/:id" component={Productpage} />
+          <Route path="/cart/:id?" component={Cartpage} />
+          <Route path="/" component={Homepage} exact />
         </Container>
       </main>
       <Footer />
